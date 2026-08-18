c5baf15fdf45b9e8c1ca15a2eb096a9cb77bc434
Aryan <aryanmhatre612@gmail.com>
2026-08-18 17:12:22 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,94 @@
+var app = angular.module("jobApp", []);
+
+app.controller("JobController", function ($scope) {
+
+    // Load data from localStorage
+    let savedApplications = JSON.parse(
+        localStorage.getItem("applications")
+    );
+
+    // If nothing saved, use default data
+    $scope.applications = savedApplications || [
+        {
+            company: "Hudl",
+            role: "Support Analyst",
+            status: "Interview Scheduled"
+        },
+        {
+            company: "TCS",
+            role: "Software Developer",
+            status: "Applied"
+        }
+    ];
+
+    $scope.totalApplications = $scope.applications.length;
+    function updateStats() {
+
+    $scope.appliedCount =
+        $scope.applications.filter(
+            app => app.status === "Applied"
+        ).length;
+
+    $scope.interviewCount =
+        $scope.applications.filter(
+            app => app.status === "Interview Scheduled"
+        ).length;
+
+    $scope.selectedCount =
+        $scope.applications.filter(
+            app => app.status === "Selected"
+        ).length;
+
+    $scope.rejectedCount =
+        $scope.applications.filter(
+            app => app.status === "Rejected"
+        ).length;
+}
+
+updateStats();
+
+    // Add application
+    $scope.addNewApplication = function () {
+
+        if (
+            !$scope.newCompany ||
+            !$scope.newRole ||
+            !$scope.newStatus
+        ) {
+            alert("Please fill all fields");
+            return;
+        }
+
+        $scope.applications.push({
+            company: $scope.newCompany,
+            role: $scope.newRole,
+            status: $scope.newStatus
+        });
+
+        localStorage.setItem(
+            "applications",
+            JSON.stringify($scope.applications)
+        );
+
+        $scope.totalApplications = $scope.applications.length;
+        updateStats();
+        $scope.newCompany = "";
+        $scope.newRole = "";
+        $scope.newStatus = "";
+    };
+
+    // Delete application
+    $scope.deleteApplication = function (index) {
+
+        $scope.applications.splice(index, 1);
+
+        localStorage.setItem(
+            "applications",
+            JSON.stringify($scope.applications)
+        );
+
+        $scope.totalApplications = $scope.applications.length;
+        updateStats();
+    };
+
+});
